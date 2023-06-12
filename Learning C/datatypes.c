a2455850d34700ae953e23ba98b5b079b5c91709
sudhanshuvishwa <sudhanshu.vishw@gmail.com>
2023-06-12 14:45:39 +0530

    learning c

--- Learning C/datatypes.c
+++ Learning C/datatypes.c
@@ -7,7 +7,7 @@ int main()
     int age = 40;
     double gpa =40.0 ;
     char grade = 'A';
-    char name[]="suddo";
+    char name[] ="suddo";
     printf("%d is age %c and %s",age,grade,name);
     return 0;
 
--- Learning C/first.c
+++ Learning C/first.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
+#include<stdlib.h> 
 int main() {
-   // printf() displays the string inside quotation
    printf("Hello, World!");
    return 0;
 }
